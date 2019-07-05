e486e9998738fb6e35a0c9f447f9297fffbcc53a
Joe Grotto <joegrotto@heir.company>
2019-07-05 09:08:28 -0500

    left class based versions commented out :)

--- src/pages/Palette.js
+++ src/pages/Palette.js
@@ -103,4 +103,83 @@ const Palette = ({ defaultPalette, palette }) => {
   );
 };
 
+// class Palette extends Component {
+//   state = {
+//     format: 'hex',
+//     level: 500,
+//     open: false
+//   };
+
+//   shouldComponentUpdate(nextProps, nextState) {
+//     const formatUnchanged = this.state.format === nextState.format;
+//     const openToggled = this.state.open !== nextState.open;
+
+//     if (formatUnchanged && openToggled) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   }
+
+//   onChangeFormat = event => this.setState({ format: event.target.value, open: true });
+
+//   onChangeLevel = level => this.setState({ level });
+
+//   onToggleOpen = () => this.setState({ open: false });
+
+//   render() {
+//     const { format, level, open } = this.state;
+//     let { defaultPalette, palette } = this.props;
+
+//     if (!palette) {
+//       palette = defaultPalette;
+//     }
+
+//     const { colors, emoji, id, paletteName } = palette;
+
+//     let colindex = 0;
+//     const colorBoxes = colors[level].map(color => {
+//       if (colindex === PaletteColumns) {
+//         colindex = 1;
+//       } else {
+//         colindex++;
+//       }
+
+//       return (
+//         <ColorBox
+//           key={color.id}
+//           boxId={color.name.toLowerCase().replace(/ /g, '-')}
+//           colindex={colindex}
+//           color={color[format]}
+//           id={color.id}
+//           name={color.name}
+//           to={`/palettes/${palette.id}/colors/${color.id}`}
+//           type={'palette'}
+//         />
+//       );
+//     });
+
+//     return (
+//       <Layout emoji={emoji} paletteName={paletteName}>
+//         <Nav id={'palette'} style={{ padding: '1rem' }}>
+//           <NavBarTitle>
+//             <strong>Palette: </strong> {paletteName}
+//           </NavBarTitle>
+//           <PaletteToolBar
+//             format={format}
+//             level={level}
+//             open={open}
+//             onChangeFormat={this.onChangeFormat}
+//             onChangeLevel={this.onChangeLevel}
+//             onToggleOpen={this.onToggleOpen}
+//           />
+//         </Nav>
+//         <Main>
+//           <PaletteColors id={id}>{colorBoxes}</PaletteColors>
+//         </Main>
+//       </Layout>
+//     );
+//   }
+// }
+
 export default Palette;
